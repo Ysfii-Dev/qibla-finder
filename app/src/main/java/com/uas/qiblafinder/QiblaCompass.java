119d8a92ac7a70293c437f24ed490241ef8bd799
Yusfi Syawali <yusfisyawali@gmail.com>
2023-12-05 09:42:30 +0700

    update

---
+++ app/src/main/java/com/uas/qiblafinder/QiblaCompass.java
@@ -0,0 +1,14 @@
+package com.uas.qiblafinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QiblaCompass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qibla_compass);
+    }
+}
