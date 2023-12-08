ddbb62c18979dc7ae99d62af39b83e7d7a7f3dc8
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-12-08 18:16:30 +0700

    feat: splash screen

---
+++ app/src/main/java/com/uas/qiblafinder/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.uas.qiblafinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
